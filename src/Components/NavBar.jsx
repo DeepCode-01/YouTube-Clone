68ef2f45a26830f9e40befecf7a78894bcc99a9a
Deepali Verma <deepaliverma064@gmail.com>
2024-10-06 15:04:08 +0530

    sidebar ui added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,13 +8,13 @@ import profileIcon from "../assets/user_profile.jpg";
 
 const NavBar = () => {
   return (
-    <nav className=" flex-div py-3 px-[2%] justify-between bg-white sticky top-0 z-10 shadow-[0_0_10px_rgba(0,0,0,0.2)]">
+    <nav className=" flex-div pb-2 px-[2%] justify-between bg-white sticky top-0 z-10 shadow-[0_0_10px_rgba(0,0,0,0.2)]">
       <div className="nav-left flex-div">
         <img className="w-5 mr-6" src={menu_con} alt="" />
         <img className="w-[130px]" src={logo} alt="" />
       </div>
       <div className="nav-middel flex-div">
-        <div className="search-box flex-div border-[1px] border-solid border-[#ccc] mt-4 py-2 px-3 rounded-[25px]" >
+        <div className="search-box flex-div border-[1px] border-solid border-[#ccc] mt-2 py-2 px-3 rounded-[25px]" >
           <input className="w-[400px] border-0 outline-0 bg-transparent" type="text" placeholder="Search" />
           <img className="w-4" src={searchIcon} alt="" />
         </div>
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,78 +1,93 @@
-import React from 'react'
-import HomeImg from "../assets/home.png"
-import gameIcon from "../assets/game_icon.png"
-import autoMobiles from "../assets/automobiles.png"
-import entertainment from "../assets/entertainment.png"
-import Tech from "../assets/tech.png"
-import Music from "../assets/music.png"
-import Blog from "../assets/blogs.png"
-import News from "../assets/news.png"
-import simon from "../assets/simon.png"
-import Tom from "../assets/tom.png"
-import Megan from "../assets/megan.png"
-import Cameron from "../assets/cameron.png"
-import Sports from "../assets/sports.png"
-
-
+import HomeImg from "../assets/home.png";
+import gameIcon from "../assets/game_icon.png";
+import autoMobiles from "../assets/automobiles.png";
+import entertainment from "../assets/entertainment.png";
+import Tech from "../assets/tech.png";
+import Music from "../assets/music.png";
+import Blog from "../assets/blogs.png";
+import News from "../assets/news.png";
+import simon from "../assets/simon.png";
+import Tom from "../assets/tom.png";
+import Megan from "../assets/megan.png";
+import Cameron from "../assets/cameron.png";
+import Jack from "../assets/jack.png";
+import Sports from "../assets/sports.png";
 
 const SideBar = () => {
   return (
-    <div className='sidebar'>
+    <div className="sidebar bg-[#fff] width-[15%] h-[10vh] fixed top-0 pl-[2%] pt-20">
       <div className="sortcut-links">
-        <div className="side-links">
-            <img src={HomeImg} alt="" /><p>Home</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img className="w-5 mr-5" src={HomeImg} alt="" />
+          <p>Home</p>
         </div>
 
-        <div className="side-links">
-            <img src={gameIcon} alt="" /><p>Gaming</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img className="w-5 mr-5"  src={gameIcon} alt="" />
+          <p>Gaming</p>
         </div>
 
-        <div className="side-links">
-            <img src={autoMobiles} alt="" /><p>Automobile</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img  className="w-5 mr-5" src={autoMobiles} alt="" />
+          <p>Automobile</p>
         </div>
 
-        <div className="side-links">
-            <img src={entertainment} alt="" /><p>Home</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img className="w-5 mr-5" src={entertainment} alt="" />
+          <p>entertainment</p>
         </div>
 
-        <div className="side-links">
-            <img src={Tech} alt="" /><p>Home</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img  className="w-5 mr-5" src={Tech} alt="" />
+          <p>Technology</p>
         </div>
 
-        <div className="side-links">
-            <img src={Music} alt="" /><p>Home</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img  className="w-5 mr-5" src={Music} alt="" />
+          <p>Music</p>
         </div>
 
-        <div className="side-links">
-            <img src={Blog} alt="" /><p>Home</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img  className="w-5 mr-5" src={Blog} alt="" />
+          <p>Blog</p>
         </div>
 
-        <div className="side-links">
-            <img src={News} alt="" /><p>Home</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img className="w-5 mr-5"  src={News} alt="" />
+          <p>News</p>
         </div>
 
-        <div className="side-links">
-            <img src={HomeImg} alt="" /><p>Home</p>
+        <div className="side-links flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+          <img  className="w-5 mr-5" src={Sports} alt="" />
+          <p>Sports</p>
+        </div>
+        <hr className="border-0 h-[1px] bg-[#ccc] w-[85%]" />
+      </div>
+      <div className="subscribed-list">
+        <h3 className="text-sm py-[10px] px-0 text-[#5a5a5a]">Subscribed</h3>
+        <div className="side-link flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+            <img className="w-6 rounded-full mr-5" src={Jack} alt="" /><p className="text-base">PewDewPie</p>
         </div>
 
-        <div className="side-links">
-            <img src={HomeImg} alt="" /><p>Home</p>
+        <div className="side-link flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+            <img className="w-6 rounded-full mr-5" src={simon} alt="" /><p>MrBeast</p>
         </div>
 
-        <div className="side-links">
-            <img src={HomeImg} alt="" /><p>Home</p>
+        <div className="side-link flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+            <img className="w-6 rounded-full mr-5"src={Tom} alt="" /><p>Justin Bieber</p>
         </div>
 
-        <div className="side-links">
-            <img src={HomeImg} alt="" /><p>Home</p>
+        <div className="side-link flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+            <img className="w-6 rounded-full mr-5" src={Megan} alt="" /><p>5 Minute Craft</p>
         </div>
 
-        <div className="side-links">
-            <img src={HomeImg} alt="" /><p>Home</p>
+        <div className="side-link flex items-center mb-3 w-fit flex-wrap cursor-pointer">
+            <img className="w-6 rounded-full mr-5" src={Cameron} alt="" /><p>Nas Dail</p>
         </div>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
