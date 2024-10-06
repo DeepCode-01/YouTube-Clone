fa0fbc1bfd43defd0305a97ae4a212a491d54063
Deepali Verma <deepaliverma064@gmail.com>
2024-10-06 16:02:12 +0530

    adding feed section

---
+++ src/Components/Feed.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import thumbnile1 from "../assets/thumbnail1.png"
+import thumbnile2 from "../assets/thumbnail2.png"
+import thumbnile3 from "../assets/thumbnail3.png"
+import thumbnile4 from "../assets/thumbnail4.png"
+import thumbnile5 from "../assets/thumbnail5.png"
+import thumbnile6 from "../assets/thumbnail6.png"
+import thumbnile7 from "../assets/thumbnail7.png"
+import thumbnile8 from "../assets/thumbnail8.png"
+
+const Feed = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Feed
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import Feed from "../../Components/Feed"
 import SideBar from "../../Components/SideBar"
 
 const Home = ({ sidebar }) => {
   return (
     <>
       <SideBar sidebar={sidebar} />
+      <div className={`container ${sidebar?"":"large-container"}`}>
+        <Feed/>
+      </div>
     </>
   )
 }
